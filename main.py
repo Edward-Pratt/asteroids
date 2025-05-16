ac36dd0dddc639b72610e7748373f9d447748bcf
Edward Pratt <edwardgwpratt@gmail.com>
2025-05-16 15:52:29 +0100

    Player can shoot and remove asteroids

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import *
 from player import Player
+from shot import Shot
 
 
 def main():
@@ -14,11 +15,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-
+    shots = pygame.sprite.Group()
 
     AsteroidField.containers = updatable
     Player.containers = (updatable, drawable)
     Asteroid.containers = (updatable, drawable, asteroids)
+    Shot.containers = (updatable, drawable, shots)
 
     asteroidField = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -35,6 +37,12 @@ def main():
             if asteroid.colliding(player):
                 print("Game Over!")
                 exit()
+
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.colliding(asteroid):
+                    shot.kill()
+                    asteroid.kill()
         screen.fill("black")
 
         for obj in drawable:
